064cb984f99e155ad46033db43e419b0ce1ab8a5
AdrianWojtowicz <adrian.wojtowicz@op.pl>
2022-06-07 20:45:39 +0200

    contact - class to hooks

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,34 +1,37 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import styles from "./ContactForm.module.css";
 import PropTypes from "prop-types";
 
-export default class ContactForm extends Component {
-  state = {
-    name: "",
-    number: "",
-  };
-  onHandleChange = (e) => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
-  onHandleSubmit = (e) => {
+const initialState = {
+  name: '',
+  number: '',
+}
+
+const ContactForm = (props) => {
+ const [data, setData] = useState(initialState);
+
+  const onHandleChange = ({ target }) => {
+    const { name, value } = target;
+    setData({ ...data, [name]: value });
+  }
+
+  const onHandleSubmit = e => {
     e.preventDefault();
-    this.props.onSubmitContact({ ...this.state });
-    this.reset();
-  };
-  reset = () => {
-    this.setState({ name: "", number: "" });
-  };
-  render() {
-    return (
-      <form onSubmit={this.onHandleSubmit} className={styles.form}>
+    props.onSubmitContact(data);
+    setData(initialState);
+  }
+
+  const { namee, numberr } = data
+  
+  return (
+      <form onSubmit={onHandleSubmit} className={styles.form}>
         <label className={styles.label}>
           <span className={styles.message}>Name</span>
           <input
             type="text"
-            onChange={this.onHandleChange}
+            onChange={onHandleChange}
             name="name"
-            value={this.state.name}
+            value={namee}
             className={styles.input}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
@@ -40,8 +43,8 @@ export default class ContactForm extends Component {
           <span className={styles.message}>Number</span>
           <input
             type="tel"
-            value={this.state.number}
-            onChange={this.onHandleChange}
+            value={numberr}
+            onChange={onHandleChange}
             name="number"
             className={styles.input}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
@@ -55,9 +58,11 @@ export default class ContactForm extends Component {
         </button>
       </form>
     );
-  }
+
 }
 
 ContactForm.propTypes = {
   onSubmitContact: PropTypes.func.isRequired,
 };
+
+export default ContactForm;
